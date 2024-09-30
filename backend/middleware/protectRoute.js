768b435fa3a6181343699d4320686e974941c373
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-10-01 03:57:08 +0530

    route protection added!

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ENV_VARS } from "../config/envVars.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies["jwt-netflix"];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not authorized, token is required" });
+    }
+
+    const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Token is invalid" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("error in protected route middleware:", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
