8b0a9ffa453f660c204ef2755cc0160443c69922
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-10-01 03:24:16 +0530

    fetching movies api!

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+
 import { ENV_VARS } from "./config/envVars.js";
-import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
 
 const app = express();
@@ -11,6 +12,7 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
