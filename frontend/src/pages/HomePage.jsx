3456ff309cb8f4dba3d768a171b3fdde4a5ffccf
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-10-01 06:09:28 +0530

    frontend setup and ui designe for login , signup pages

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div className="hero-bg h-screen">HomePage</div>;
+};
+
+export default HomePage;
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const SignUpPage = () => {
+  const { searchParams } = new URL(document.location);
+  const emailValue = searchParams.get("email");
+
+  const [email, setEmail] = useState(emailValue || "");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(email, username, password);
+  };
+
+  return (
+    <div className="h-screen w-full hero-bg">
+      <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+        <Link to={"/"}>
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
+        </Link>
+      </header>
+
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
+          <h1 className="text-center text-white text-2xl font-bold mb-4">
+            Sign Up
+          </h1>
+          <form className="space-y-4" onSubmit={handleSignUp}>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="w-full px-3 py-2 mt-1 border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+                placeholder="you@.com"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="username"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                className="w-full px-3 py-2 mt-1 border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+                placeholder="Dav But"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                className="w-full px-3 py-2 mt-1 border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+                placeholder="••••••••"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
+              Sign Up
+            </button>
+          </form>
+          <div className="text-center text-gray-400">
+            Already a member?{" "}
+            <Link to={"/login"} className="text-red-500 hover:underline">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
